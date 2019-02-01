d5efe83e64d2533ee725d3abf25f92c28a20f249
Tunde Osborne <osbornetunde@gmail.com>
2019-02-01 21:22:47 +0100

    feat(edit a party): -update a party information and it should reflect in the database -prevent incomplete data from getting into the database [Delivers #163585679]

--- src/controllers/parties.js
+++ src/controllers/parties.js
@@ -76,6 +76,56 @@ class PartiesController {
         });
     }
 
+    editAParty(req, res) {
+        const id = parseInt(req.params.id);
+        let partyFound;
+        let partyIndex;
+        parties.map((party, index) => {
+            if(party.id === id) {
+                partyFound = party;
+                partyIndex = index;
+            }
+        });
+    
+    if(!partyFound) {
+        return res.status(404).json({
+            status: '404',
+            error: 'party not found'
+        });
+    }
+    
+    if(!req.body.name) {
+        return res.status(400).json({
+            status: '400',
+            error: 'party name is required'
+        });
+    } else if(!req.body.hqAddress) {
+        return res.status(400).json({
+            status: '400',
+            error: 'party headquarters Address required'
+        });
+    } else if(!req.body.logoUrl) {
+        return res.status(400).json({
+            status: '400',
+            error: 'party logo url is required'
+        });
+    }
+    
+    const editedparty = {
+        id: partyFound.id,
+        name: req.body.name || partyFound.name,
+        hqAddress: req.body.hqAddress || partyFound.hqAddress,
+        logoUrl: req.body.logoUrl || partyFound.logoUrl
+    };
+    
+    parties.splice(partyIndex, 1, editedparty);
+    
+    return res.status(201).json({
+        status: '201',
+        data: editedparty
+    });
+    }
+
 }
 
     
